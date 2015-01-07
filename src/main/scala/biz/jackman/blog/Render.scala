f6ea5ea532d7e1d891cb95070d003cb9e81efd63
Ben Jackman <ben@jackman.biz>
2015-01-07 13:46:04 -0600

    Stuck at title
    Signed-off-by: Ben Jackman <ben@jackman.biz>

---
+++ src/main/scala/biz/jackman/blog/Render.scala
@@ -0,0 +1,18 @@
+package biz.jackman.blog
+
+
+//////////////////////////////////////////////////////////////
+// Copyright (c) 2015 Ben Jackman, Jeff Gomberg
+// All Rights Reserved
+// please contact ben@jackman.biz or jeff@cgtanalytics.com
+// for licensing inquiries
+// Created by bjackman @ 1/7/15 1:22 PM
+//////////////////////////////////////////////////////////////
+
+object Render {
+
+  def main(args : Array[String]) {
+    println(Hello().render)
+  }
+
+}
